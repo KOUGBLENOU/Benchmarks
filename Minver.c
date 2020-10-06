8224bbd3d4e5896a45b2f15c93d76ba3325cce25
KOUGBLENOU <60573398+KOUGBLENOU@users.noreply.github.com>
2020-10-06 16:39:43 +0200

    Update Minver.c

--- Minver.c
+++ Minver.c
@@ -5,39 +5,7 @@
 /* MDH WCET BENCHMARK SUITE */
 
 
-/* 2012/09/28, Jan Gustafsson <jan.gustafsson@mdh.se>
- * Changes:
- *  - Missing braces around initialization of subobject added
- *  - This program redefines the standard C function fabs. Therefore, the
- *  function has been renamed to minver_fabs.
- */
-
-/*************************************************************************/
-/*                                                                       */
-/*   SNU-RT Benchmark Suite for Worst Case Timing Analysis               */
-/*   =====================================================               */
-/*                              Collected and Modified by S.-S. Lim      */
-/*                                           sslim@archi.snu.ac.kr       */
-/*                                         Real-Time Research Group      */
-/*                                        Seoul National University      */
-/*                                                                       */
-/*                                                                       */
-/*        < Features > - restrictions for our experimental environment   */
-/*                                                                       */
-/*          1. Completely structured.                                    */
-/*               - There are no unconditional jumps.                     */
-/*               - There are no exit from loop bodies.                   */
-/*                 (There are no 'break' or 'return' in loop bodies)     */
-/*          2. No 'switch' statements.                                   */
-/*          3. No 'do..while' statements.                                */
-/*          4. Expressions are restricted.                               */
-/*               - There are no multiple expressions joined by 'or',     */
-/*                'and' operations.                                      */
-/*          5. No library calls.                                         */
-/*               - All the functions needed are implemented in the       */
-/*                 source file.                                          */
-/*                                                                       */
-/*                                                                       */
+                                                */
 /*************************************************************************/
 /*                                                                       */
 /*  FILE: minver.c                                                       */
